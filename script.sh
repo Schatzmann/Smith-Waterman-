8890e402b2c166ab9387211432a9ddb461a46429
Eduardo Zimermam <eduardozimerman@live.com>
2019-06-12 21:00:15 -0300

    Alteração do script de teste para entrega final

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ echo "Iniciando o processamento em Intel(R) Core(TM) i5-7500 CPU @ 3.40GHz"
 echo -e
 
 
-# # ------------------------------ TRECHO DE EXECUÇÃO PARA SEQUENCIAL N (6K) --------------------------------
+# ------------------------------ TRECHO DE EXECUÇÃO PARA SEQUENCIAL N (6K) --------------------------------
 
 
 echo "EXECUÇÃO DAS SEQUENCIAS COM TAMANHO N"
@@ -39,7 +39,27 @@ echo "Executando os paralelos 20 vezes para 6k"
 for i in {1..20}; do ./smithParalelo examples/SeqArua6k.txt 30; done > resultados/paraleloArua_6k_4th.txt
 for i in {1..20}; do ./smithParalelo examples/SeqMouse6k.txt 30; done > resultados/paraleloMouse_6k_4th.txt
 
+echo -e
 
+export OMP_NUM_THREADS=8
+
+echo "Processamento paralelo sendo feito com $OMP_NUM_THREADS threads"
+
+echo "Executando os paralelos 20 vezes para 6k"
+for i in {1..20}; do ./smithParalelo examples/SeqArua6k.txt 30; done > resultados/paraleloArua_6k_8th.txt
+for i in {1..20}; do ./smithParalelo examples/SeqMouse6k.txt 30; done > resultados/paraleloMouse_6k_8th.txt
+
+
+
+echo -e
+
+export OMP_NUM_THREADS=16
+
+echo "Processamento paralelo sendo feito com $OMP_NUM_THREADS threads"
+
+echo "Executando os paralelos 20 vezes para 6k"
+for i in {1..20}; do ./smithParalelo examples/SeqArua6k.txt 30; done > resultados/paraleloArua_6k_16th.txt
+for i in {1..20}; do ./smithParalelo examples/SeqMouse6k.txt 30; done > resultados/paraleloMouse_6k_16th.txt
 
 
 # ------------------------------ TRECHO DE EXECUÇÃO PARA SEQUENCIAL 2N (13K) --------------------------------
@@ -74,7 +94,30 @@ echo "Executando 20 vezes o paralelo 13k"
 for i in {1..20}; do ./smithParalelo examples/SeqArua13k.txt 30; done > resultados/paraleloArua_13k_4th.txt
 for i in {1..20}; do ./smithParalelo examples/SeqMouse13k.txt 30; done > resultados/paraleloMouse_13k_4th.txt
 
+echo -e
 
+export OMP_NUM_THREADS=8
+
+echo "Processamento paralelo sendo feito com $OMP_NUM_THREADS threads"
+
+echo -e
+
+echo "Executando 20 vezes o paralelo 13k"
+for i in {1..20}; do ./smithParalelo examples/SeqArua13k.txt 30; done > resultados/paraleloArua_13k_8th.txt
+for i in {1..20}; do ./smithParalelo examples/SeqMouse13k.txt 30; done > resultados/paraleloMouse_13k_8th.txt
+
+
+echo -e
+
+export OMP_NUM_THREADS=16
+
+echo "Processamento paralelo sendo feito com $OMP_NUM_THREADS threads"
+
+echo -e
+
+echo "Executando 20 vezes o paralelo 13k"
+for i in {1..20}; do ./smithParalelo examples/SeqArua13k.txt 30; done > resultados/paraleloArua_13k_16th.txt
+for i in {1..20}; do ./smithParalelo examples/SeqMouse13k.txt 30; done > resultados/paraleloMouse_13k_16th.txt
 
 # ------------------------------ TRECHO DE EXECUÇÃO PARA SEQUENCIAL 4N (26K) --------------------------------
 
@@ -97,7 +140,6 @@ for i in {1..20}; do ./smithParalelo examples/SeqMouse26k.txt 30; done > resulta
 
 echo -e
 
-
 export OMP_NUM_THREADS=4
 
 echo "Processamento paralelo sendo feito com $OMP_NUM_THREADS threads"
@@ -107,3 +149,28 @@ echo -e
 echo "Executando 20 vezes o paralelo 26k"
 for i in {1..20}; do ./smithParalelo examples/SeqArua26k.txt 30; done > resultados/paraleloArua_26k_4th.txt
 for i in {1..20}; do ./smithParalelo examples/SeqMouse26k.txt 30; done > resultados/paraleloMouse_26k_4th.txt
+
+
+echo -e
+
+export OMP_NUM_THREADS=8
+
+echo "Processamento paralelo sendo feito com $OMP_NUM_THREADS threads"
+
+echo -e
+
+echo "Executando 20 vezes o paralelo 26k"
+for i in {1..20}; do ./smithParalelo examples/SeqArua26k.txt 30; done > resultados/paraleloArua_26k_8th.txt
+for i in {1..20}; do ./smithParalelo examples/SeqMouse26k.txt 30; done > resultados/paraleloMouse_26k_8th.txt
+
+echo -e
+
+export OMP_NUM_THREADS=16
+
+echo "Processamento paralelo sendo feito com $OMP_NUM_THREADS threads"
+
+echo -e
+
+echo "Executando 20 vezes o paralelo 26k"
+for i in {1..20}; do ./smithParalelo examples/SeqArua26k.txt 30; done > resultados/paraleloArua_26k_16th.txt
+for i in {1..20}; do ./smithParalelo examples/SeqMouse26k.txt 30; done > resultados/paraleloMouse_26k_16th.txt
